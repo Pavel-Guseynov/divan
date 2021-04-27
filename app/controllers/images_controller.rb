259b96e815fd39d6908c97a676fb5a31c4652c57
Pavel Guseynov <pavel@didlogic.com>
2021-04-27 23:31:17 +0300

    cleanup

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,1 +1,21 @@
+class ImagesController < ApplicationController
+  before_action :set_image, except: :index
 
+  def index
+    @images  = Image.all
+  end
+
+  def show
+
+  end
+
+  def file
+    send_file @image.path
+  end
+
+  private
+
+  def set_image
+    @image = Image.find params[:id]
+  end
+end
