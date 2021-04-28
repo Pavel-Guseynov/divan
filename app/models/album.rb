69da7cb5408f5ee28b67c2e31f16337263bfab34
Pavel Guseynov <pavel@didlogic.com>
2021-04-28 21:54:44 +0300

    more image tuning

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class Album < ApplicationRecord
-  ROOT_IMAGES_DIR = '/Users/pavelguseynov/Downloads/vr_images'
-
   has_many :images, dependent: :destroy
 
-  def self.import
-    Dir["#{ROOT_IMAGES_DIR}/**/"].each do |filename|
+  def self.import(dir)
+    Dir["#{dir}/**/"].each do |filename|
       album = find_or_create_by name: File.basename(filename), path: filename
 
       Image.import filename, album
